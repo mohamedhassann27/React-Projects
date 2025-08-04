e65dc3bbe70367edfd8ee32b512bd7cce192c7f0
mohamed hassan <141786275+mohamedhassann27@users.noreply.github.com>
2025-08-04 11:38:35 +0300

    Update Subscripe.jsx

--- Project-2 (Shopsy Ecommerce)/src/components/Subscripe/Subscripe.jsx
+++ Project-2 (Shopsy Ecommerce)/src/components/Subscripe/Subscripe.jsx
@@ -1,6 +1,6 @@
 function Subscripe() {
     return (
-        <section data-aos='zoom-in' className="bg-[url('orange-pattern.jpg')] p-10 max-sm:px-0 bg-cover bg-center ">
+        <section data-aos='zoom-in' className="bg-[url('/orange-pattern.jpg')] p-10 max-sm:px-0 bg-cover bg-center ">
             <div className=" flex flex-col items-center justify-center gap-6 w-[80%] max-w-150 m-auto backdrop-blur-sm">
                 <h2 className="text-4xl max-sm:text-3xl font-semibold text-white">Get Notified About New Products</h2>
                 <input data-aos='fade-up' type="email" placeholder="Enter your email" className="bg-white rounded-sm p-3 w-full"/>
@@ -10,3 +10,4 @@ function Subscripe() {
 }
 
 export default Subscripe
+
